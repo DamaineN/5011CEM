b4bc429ee3c27d020f1a3dd8b5d00e1749220e7a
Daniel Neoh <damaine334@gmail.com>
2025-07-02 13:45:58 +0800

    Update Dashboard.py
    Started code implementation by adding imports and session initialization

--- Dashboard.py
+++ Dashboard.py
@@ -1,1 +1,18 @@
+# Leading Causes of Death Dashboard using ARIMA
+
+import pandas as pd
+import streamlit as st
+import plotly.express as px
+from pmdarima import auto_arima
+import warnings
+
+warnings.filterwarnings("ignore")
+
+st.set_page_config(page_title="Leading Causes of Death Dashboard", layout="wide")
+
+# Session Initialization
+if 'data_uploaded' not in st.session_state:
+    st.session_state['data_uploaded'] = False
+if 'data' not in st.session_state:
+    st.session_state['data'] = None
 
